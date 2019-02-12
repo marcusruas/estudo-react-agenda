790686e93b20b2083b4a0993e36bc778dcf4482b
marcusruas <marcus.ruas@bol.com.br>
2019-02-11 23:02:13 -0200

    criado layout para o projeto

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './static/index.css';
+import Layout from './layout';
 import * as serviceWorker from './services/serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 serviceWorker.register();
+
+ReactDOM.render(<App />, document.getElementById('root'));
